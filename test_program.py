8c5aa55fb541782702177917843f45d207eedb50
Владимир Орехов <vo56514@yandex.ru>
2023-03-24 16:47:55 +0300

    Fixed test_program.py

--- test_program.py
+++ test_program.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -5,16 +6,10 @@ dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 
 files_list = ['program.py', 'readme.md']
 
-
 def test_program():
     for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
-
+        assert filename in dir_files, "Файл {filename} не найден в корне репозитория"
     try:
         import program
     except Exception as e:
-        assert False, (
-            'Не удалось запустить `program.py`. '
-            'Исправьте в нем ошибки:\n'
-            f'{e}'
-        )
+        assert "Не удалось запустить program.py.\n Исправьте в нем ошибки:\n {e}"
